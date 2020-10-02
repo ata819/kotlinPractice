95a9807adfc95651a5ddb96db3fe725018ca36b5
Alan Ta <alanta@Alans-MBP.san.rr.com>
2020-10-01 18:24:45 -0700

    Updated the basic Kotlin practice with fireball spell and inebriation status

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
     val isBlessed = true
     val auraVisible = isBlessed && Health > 50 || isImortal
     val auraColor = if (auraVisible) "Green" else "None"
-    println(auraColor)
+
     val factions = when (race){
         "dwarf" -> "Keepers of the Mines"
         "gnome" -> "Keepers of the Mines"
@@ -36,12 +36,41 @@ fun main(args: Array<String>) {
      */
     // Rule of thumb: Used "when" to replace "else if" loops
     val healthStatus = formatHeathStatus(Health, isBlessed)
-    println("$name $healthStatus")
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+    castFireball()
 
 }
 
-private fun formatHeathStatus(Health: Int, isBlessed: Boolean): String {
-    val healthStatus = when (Health) {
+private fun castFireball(numFireballs: Int = 2) {
+    println("A glass of Fireball springs into existence (x$numFireballs)")
+    val inebriation = (1 .. 50).random()
+    val inebriationStatus = when (inebriation){
+        in 41..50 -> "t0aSt3d"
+        in 31..40 -> "stewed"
+        in 21..30 -> "soused"
+        in 11..20 -> "sloshed"
+        else -> "tipsy"
+    }
+    println(inebriationStatus)
+
+}
+
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor) " +
+                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+    )
+    println("$name $healthStatus")
+}
+
+private fun formatHeathStatus(Health: Int, isBlessed: Boolean) =
+     when (Health) {
         100 -> "is in excellent condition"
         in 65..89 -> if (isBlessed) {
             "is in a decent condition but can heal quickly"
@@ -51,5 +80,3 @@ private fun formatHeathStatus(Health: Int, isBlessed: Boolean): String {
         in 35..64 -> "is not in any good condition"
         else -> "is in awful condition"
     }
-    return healthStatus
-}
